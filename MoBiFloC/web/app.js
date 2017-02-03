d66c83877bdc44970a41eba67266a23ae59c96fd
makatony <makatony@gmail.com>
2017-02-03 12:26:14 +0100

    added SDP610 pressure sensor library files

--- MoBiFloC/web/app.js
+++ MoBiFloC/web/app.js
@@ -1,19 +1,9 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 require('dotenv').config({
 	path: "envVars.env"
 });
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
@@ -29,3 +19,46 @@ var server = app.listen(appEnv.port, '0.0.0.0', function () {
 		// print a message when the server starts listening
 		console.log("server starting on " + appEnv.url);
 	});
+	
+	
+var ttn = require('ttn');
+
+var region = 'eu';
+var appId = '';
+var accessKey = '';
+
+var client = new ttn.Client(region, appId, accessKey);
+
+client.on('connect', function(connack) {
+  console.log('[DEBUG]', 'Connect:', connack);
+});
+
+client.on('error', function(err) {
+  console.error('[ERROR]', err.message);
+});
+
+client.on('activation', function(deviceId, data) {
+  console.log('[INFO] ', 'Activation:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('device', null, 'down/scheduled', function(deviceId, data) {
+  console.log('[INFO] ', 'Scheduled:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('message', function(deviceId, data) {
+  console.info('[INFO] ', 'Message:', deviceId, JSON.stringify(data, null, 2));
+});
+
+client.on('message', null, 'led', function(deviceId, led) {
+
+  // Toggle the LED
+  var payload = {
+    led: !led
+  };
+
+  // If you don't have an encoder payload function:
+  // var payload = [led ? 0 : 1];
+
+  console.log('[DEBUG]', 'Sending:', JSON.stringify(payload));
+  client.send(deviceId, payload);
+});
