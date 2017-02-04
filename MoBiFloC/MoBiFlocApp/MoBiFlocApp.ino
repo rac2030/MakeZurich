19a4cf3fb77cb368d3deaa2d9c764d5f2c55663b
rac2030 <m@rac.su>
2017-02-04 05:06:00 +0100

    Successfully sent data from SDP610 over LoRaWAN

--- MoBiFloC/MoBiFlocApp/MoBiFlocApp.ino
+++ MoBiFloC/MoBiFlocApp/MoBiFlocApp.ino
@@ -1,5 +1,9 @@
-#include <rn2xx3.h>
+#include <rn2xx3.h> // Library for the LoRaWAN module RN2438 from microchip
 #include <SoftwareSerial.h>
+#include <Wire.h> // Needed for i2c
+#include "SDP6x.h" // Library to read from SDP610 differential presure sensor
+
+float difPressure; // Variable that holds the last reading of the SDP610
 
 SoftwareSerial mySerial(7, 8); // RX, TX
 #define RST  2
@@ -38,22 +42,50 @@ void setup() {
   Serial.print("RN2483 version number: ");
   Serial.println(myLora.sysver());
 
-  myLora.initABP(devAddr, appSKey, nwkSKey);
+  //configure your keys and join the network
+  Serial.println("Trying to join TTN");
+  bool join_result = false;
 
+  join_result = myLora.initABP(devAddr, appSKey, nwkSKey);
+  while(!join_result)
+  {
+    Serial.println("Unable to join. Are your keys correct, and do you have TTN coverage?");
+    delay(60000); //delay a minute before retry
+    join_result = myLora.init();
+  }
+  Serial.println("Successfully joined TTN");
+  
   led_off();
+
+  // Initialize I2C communication
+  Wire.begin();
+  // Set the starting value for the pressure difference to 0
+  difPressure = 0.0;
   delay(2000);
+  
 }
 
-// the loop routine runs over and over again forever:
+// Just for testing sending a float
+static uint8_t floatdata[] = { 0,0,0,0,0,0,0,0};
+
+// the loop routine runs over and over again forever
 void loop() {
   led_on();
-  Serial.println("TXing");
-
-  myLora.txUncnf("X");
-
+  readPressure();
+  Serial.print("TXing: ");
+  Serial.println(difPressure);
+  dtostrf(difPressure,5,2,(char*)floatdata);
+  myLora.txUncnf(floatdata); // non blocking call
+  //myLora.txCnf(difPressure);    // blocking call
+  
   led_off();
 
   delay(20000);
+}
+
+
+void readPressure() {
+  difPressure = SDP6x.GetPressureDiff();
 }
 
 void led_on()
